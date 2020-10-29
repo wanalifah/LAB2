030b345433e88d52fb7bbca2d24d908d5b14e939
wanalifah <2018250836@isiswa.uitm.edu.my>
2020-10-29 18:46:57 +0000

    question 2.2

--- fork2.2b.c
+++ fork2.2b.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>  /* needed to define exit() */
 #include <unistd.h>  /* needed for fork() and getpid() */
 #include <stdio.h>   /* needed for printf() */
+#include <sys/wait.h>
 
 int main(int argc, char**argv)
 {
@@ -12,6 +13,7 @@ int main(int argc, char**argv)
 		break;
 
 	default:	/* a fork returns a pid to the parent */
+		wait(NULL);
 		printf("I am the parent process: pid=%d, child pid=%d\n", getpid(), pid);
 		break;
 
